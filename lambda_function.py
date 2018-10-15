4ca4ff706af8727a2055c43e20e566d1dd2fec51
Katsumata Ryo <katsumata@stores.jp>
2018-10-16 01:07:17 +0900

    slack command post from lambda(legacy token/use depricated command)

---
+++ lambda_function.py
@@ -0,0 +1,19 @@
+from slackclient import SlackClient
+import os
+
+def lambda_handler(event, context):
+    slack_token = os.environ['TOKEN_ID']
+    sc = SlackClient(slack_token)
+
+    sc.api_call(
+        "chat.command",
+        channel="CAxxxxxx",
+        command='/slach_command',
+    )
+    
+    return {
+        'isBase64Encoded': False,
+        'statusCode': 200,
+        'headers': {},
+        'body': '{"message": "Hello from AWS Lambda"}'
+    }
